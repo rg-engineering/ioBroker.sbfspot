ed0dcffdbb429b81e92a4d3248758c41d6360cad
Rene <info@rg-engineering.eu>
2017-03-28 19:44:04 +0200

    first release 0.1.0

--- myhomecontrol_sbfspot.js
+++ myhomecontrol_sbfspot.js
@@ -152,8 +152,15 @@ function GetInverters() {
             AddInverterVariables(rows[0].Serial);
 
             adapter.setState( rows[0].Serial + ".Type", { ack: true, val: rows[0].Type});
-            adapter.setState( rows[0].Serial + ".EToday", { ack: true, val: rows[0].EToday });
-            adapter.setState( rows[0].Serial + ".ETotal", { ack: true, val: rows[0].ETotal });
+            //adapter.setState( rows[0].Serial + ".EToday", { ack: true, val: rows[0].EToday }); this is kW
+            //adapter.setState(rows[0].Serial + ".ETotal", { ack: true, val: rows[0].ETotal }); this is kW
+            adapter.setState(rows[0].Serial + ".SW_Version", { ack: true, val: rows[0].SW_Version });
+            adapter.setState(rows[0].Serial + ".TotalPac", { ack: true, val: rows[0].TotalPac });
+            adapter.setState(rows[0].Serial + ".OperatingTime", { ack: true, val: rows[0].OperatingTime });
+            adapter.setState(rows[0].Serial + ".FeedInTime", { ack: true, val: rows[0].FeedInTime });
+            adapter.setState(rows[0].Serial + ".Status", { ack: true, val: rows[0].Status });
+            adapter.setState(rows[0].Serial + ".GridRelay", { ack: true, val: rows[0].GridRelay });
+            adapter.setState(rows[0].Serial + ".Temperature", { ack: true, val: rows[0].Temperature });
 
             GetInvertersData(rows[0].Serial);
         }
@@ -170,6 +177,27 @@ function GetInvertersData(serial) {
         if (!err) {
             adapter.log.debug('rows ' + JSON.stringify(rows));
 
+            adapter.setState(rows[0].Serial + ".Pdc1", { ack: true, val: rows[0].Pdc1 });
+            adapter.setState(rows[0].Serial + ".Pdc2", { ack: true, val: rows[0].Pdc2 });
+            adapter.setState(rows[0].Serial + ".Idc1", { ack: true, val: rows[0].Idc1 });
+            adapter.setState(rows[0].Serial + ".Idc2", { ack: true, val: rows[0].Idc2 });
+            adapter.setState(rows[0].Serial + ".Udc1", { ack: true, val: rows[0].Udc1 });
+            adapter.setState(rows[0].Serial + ".Udc2", { ack: true, val: rows[0].Udc2 });
+
+            adapter.setState(rows[0].Serial + ".Pac1", { ack: true, val: rows[0].Pac1 });
+            adapter.setState(rows[0].Serial + ".Pac2", { ack: true, val: rows[0].Pac2 });
+            adapter.setState(rows[0].Serial + ".Pac3", { ack: true, val: rows[0].Pac3 });
+            adapter.setState(rows[0].Serial + ".Iac1", { ack: true, val: rows[0].Iac1 });
+            adapter.setState(rows[0].Serial + ".Iac2", { ack: true, val: rows[0].Iac2 });
+            adapter.setState(rows[0].Serial + ".Iac3", { ack: true, val: rows[0].Iac3 });
+            adapter.setState(rows[0].Serial + ".Uac1", { ack: true, val: rows[0].Uac1 });
+            adapter.setState(rows[0].Serial + ".Uac2", { ack: true, val: rows[0].Uac2 });
+            adapter.setState(rows[0].Serial + ".Uac3", { ack: true, val: rows[0].Uac3 });
+
+            adapter.setState(rows[0].Serial + ".EToday", { ack: true, val: rows[0].EToday });
+            adapter.setState(rows[0].Serial + ".ETotal", { ack: true, val: rows[0].ETotal });
+            adapter.setState(rows[0].Serial + ".Frequency", { ack: true, val: rows[0].Frequency });
+            adapter.setState(rows[0].Serial + ".BT_Signal", { ack: true, val: rows[0].BT_Signal });
 
             Disconnect();
         }
@@ -210,14 +238,145 @@ function AddInverterVariables(serial) {
 
     adapter.setObjectNotExists( serial + '.ETotal', {
         type: 'state',
-        common: { name: 'SMA inverter Ertrag Total', type: 'number', role: 'ertrag', unit: 'kW', read: true, write: false },
+        common: { name: 'SMA inverter Ertrag Total', type: 'number', role: 'ertrag', unit: 'W', read: true, write: false },
         native: { location:  serial + '.ETotal' }
     });
 
     adapter.setObjectNotExists( serial + '.EToday', {
         type: 'state',
-        common: { name: 'SMA inverter Ertrag Today', type: 'number', role: 'ertrag', unit: 'kW', read: true, write: false },
+        common: { name: 'SMA inverter Ertrag Today', type: 'number', role: 'ertrag', unit: 'W', read: true, write: false },
         native: { location:  serial + '.EToday' }
+    });
+
+    adapter.setObjectNotExists(serial + '.SW_Version', {
+        type: 'state',
+        common: { name: 'SMA inverter SW Version', type: 'string', role: 'version', unit: '', read: true, write: false },
+        native: { location: serial + '.SW_Version' }
+    });
+
+    adapter.setObjectNotExists(serial + '.TotalPac', {
+        type: 'state',
+        common: { name: 'SMA inverter Total P ac', type: 'number', role: 'ertrag', unit: '', read: true, write: false },
+        native: { location: serial + '.TotalPac' }
+    });
+
+    adapter.setObjectNotExists(serial + '.OperatingTime', {
+        type: 'state',
+        common: { name: 'SMA inverter Operating Time', type: 'number', role: 'ertrag', unit: 'h', read: true, write: false },
+        native: { location: serial + '.OperatingTime' }
+    });
+
+    adapter.setObjectNotExists(serial + '.FeedInTime', {
+        type: 'state',
+        common: { name: 'SMA inverter Feed In Time', type: 'number', role: 'ertrag', unit: 'h', read: true, write: false },
+        native: { location: serial + '.FeedInTime' }
+    });
+
+    adapter.setObjectNotExists(serial + '.Status', {
+        type: 'state',
+        common: { name: 'SMA inverter Status', type: 'string', role: 'ertrag', unit: '', read: true, write: false },
+        native: { location: serial + '.FeedInTime' }
+    });
+
+    adapter.setObjectNotExists(serial + '.GridRelay', {
+        type: 'state',
+        common: { name: 'SMA inverter Status', type: 'string', role: 'ertrag', unit: '', read: true, write: false },
+        native: { location: serial + '.GridRelay' }
+    });
+
+    adapter.setObjectNotExists(serial + '.Temperature', {
+        type: 'state',
+        common: { name: 'SMA inverter Status', type: 'number', role: 'ertrag', unit: '°C', read: true, write: false },
+        native: { location: serial + '.Temperature' }
+    });
+
+    adapter.setObjectNotExists(serial + '.Pdc1', {
+        type: 'state',
+        common: { name: 'SMA inverter Power DC 1', type: 'number', role: 'ertrag', unit: 'W', read: true, write: false },
+        native: { location: serial + '.Pdc1' }
+    });
+    adapter.setObjectNotExists(serial + '.Pdc2', {
+        type: 'state',
+        common: { name: 'SMA inverter Power DC 2', type: 'number', role: 'ertrag', unit: 'W', read: true, write: false },
+        native: { location: serial + '.Pdc2' }
+    });
+    adapter.setObjectNotExists(serial + '.Idc1', {
+        type: 'state',
+        common: { name: 'SMA inverter Current DC 1', type: 'number', role: 'ertrag', unit: 'A', read: true, write: false },
+        native: { location: serial + '.Idc1' }
+    });
+    adapter.setObjectNotExists(serial + '.Idc2', {
+        type: 'state',
+        common: { name: 'SMA inverter Current DC 2', type: 'number', role: 'ertrag', unit: 'A', read: true, write: false },
+        native: { location: serial + '.Idc2' }
+    });
+    adapter.setObjectNotExists(serial + '.Udc1', {
+        type: 'state',
+        common: { name: 'SMA inverter Voltage DC 1', type: 'number', role: 'ertrag', unit: 'V', read: true, write: false },
+        native: { location: serial + '.Udc1' }
+    });
+    adapter.setObjectNotExists(serial + '.Udc2', {
+        type: 'state',
+        common: { name: 'SMA inverter Voltage DC 2', type: 'number', role: 'ertrag', unit: 'V', read: true, write: false },
+        native: { location: serial + '.Udc2' }
+    });
+
+
+    adapter.setObjectNotExists(serial + '.Pac1', {
+        type: 'state',
+        common: { name: 'SMA inverter Power AC 1', type: 'number', role: 'ertrag', unit: 'W', read: true, write: false },
+        native: { location: serial + '.Pac1' }
+    });
+    adapter.setObjectNotExists(serial + '.Pac2', {
+        type: 'state',
+        common: { name: 'SMA inverter Power AC 2', type: 'number', role: 'ertrag', unit: 'W', read: true, write: false },
+        native: { location: serial + '.Pac2' }
+    });
+    adapter.setObjectNotExists(serial + '.Pac3', {
+        type: 'state',
+        common: { name: 'SMA inverter Power AC 3', type: 'number', role: 'ertrag', unit: 'W', read: true, write: false },
+        native: { location: serial + '.Pac3' }
+    });
+    adapter.setObjectNotExists(serial + '.Iac1', {
+        type: 'state',
+        common: { name: 'SMA inverter Current AC 1', type: 'number', role: 'ertrag', unit: 'A', read: true, write: false },
+        native: { location: serial + '.Iac1' }
+    });
+    adapter.setObjectNotExists(serial + '.Iac2', {
+        type: 'state',
+        common: { name: 'SMA inverter Current AC 2', type: 'number', role: 'ertrag', unit: 'A', read: true, write: false },
+        native: { location: serial + '.Iac2' }
+    });
+    adapter.setObjectNotExists(serial + '.Iac3', {
+        type: 'state',
+        common: { name: 'SMA inverter Current AC 3', type: 'number', role: 'ertrag', unit: 'A', read: true, write: false },
+        native: { location: serial + '.Iac3' }
+    });
+    adapter.setObjectNotExists(serial + '.Uac1', {
+        type: 'state',
+        common: { name: 'SMA inverter Voltage AC 1', type: 'number', role: 'ertrag', unit: 'V', read: true, write: false },
+        native: { location: serial + '.Uac1' }
+    });
+    adapter.setObjectNotExists(serial + '.Uac2', {
+        type: 'state',
+        common: { name: 'SMA inverter Voltage AC 2', type: 'number', role: 'ertrag', unit: 'V', read: true, write: false },
+        native: { location: serial + '.Uac2' }
+    });
+    adapter.setObjectNotExists(serial + '.Uac3', {
+        type: 'state',
+        common: { name: 'SMA inverter Voltage AC 3', type: 'number', role: 'ertrag', unit: 'V', read: true, write: false },
+        native: { location: serial + '.Uac3' }
+    });
+
+    adapter.setObjectNotExists(serial + '.Frequency', {
+        type: 'state',
+        common: { name: 'SMA inverter Frequency', type: 'number', role: 'ertrag', unit: 'Hz', read: true, write: false },
+        native: { location: serial + '.Frequency' }
+    });
+    adapter.setObjectNotExists(serial + '.BT_Signal', {
+        type: 'state',
+        common: { name: 'SMA inverter BT_Signal', type: 'number', role: 'ertrag', unit: '%', read: true, write: false },
+        native: { location: serial + '.BT_Signal' }
     });
 
 }
@@ -249,3 +408,33 @@ var rows =
 rows[0].Serial
 
 */
+
+var rows = [
+    {
+        'TimeStamp': 1490714113,
+        'Serial': 2000562095,
+        'Pdc1': 561,
+        'Pdc2': 0,
+        'Idc1': 2.358,
+        'Idc2': 0,
+        'Udc1': 238,
+        'Udc2': 0,
+        'Pac1': 538,
+        'Pac2': 0,
+        'Pac3': 0,
+        'Iac1': 2.281,
+        'Iac2': 0,
+        'Iac3': 0,
+        'Uac1': 236,
+        'Uac2': 0,
+        'Uac3': 0,
+        'EToday': 13906,
+        'ETotal': 18355308,
+        'Frequency': 50.05,
+        'OperatingTime': 34909.2,
+        'FeedInTime': 29500.3,
+        'BT_Signal': 76.8627,
+        'Status': 'OK',
+        'GridRelay': '?',
+        'Temperature': 0
+    }]
