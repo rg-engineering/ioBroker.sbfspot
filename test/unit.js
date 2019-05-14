2ba03589e718167ff4c542a2297e4e741354d525
dominic.griesel <d.griesel@gmx.net>
2019-05-14 16:11:01 +0200

    add-mocks

--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,29 @@
 const path = require('path');
 const { tests } = require('@iobroker/testing');
 
+const mysqlMock = {
+	createConnection() {
+		return {
+			connect() { },
+			query() { },
+			end() { },
+		};
+	}
+}
+
+const sqliteMock = {
+	verbose() { return sqliteMock },
+	Database: class {
+		constructor() { }
+		all() { }
+		close() { }
+	}
+}
+
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, '..'), { allowedExitCodes: [11] });
+tests.unit(path.join(__dirname, '..'), {
+	allowedExitCodes: [11], additionalMockedModules: {
+		"mysql": mysqlMock,
+		"sqlite3": sqliteMock,
+	}
+});
