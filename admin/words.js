93b81688a7c355c81d70cafa7f5734c090c90454
Rene Glaß <info@rg-engineering.eu>
2021-05-13 11:10:31 +0200

    test node 16

--- admin/words.js
+++ admin/words.js
@@ -1,185 +1,20 @@
-﻿/*global systemDictionary:true */
+/*global systemDictionary:true */
 'use strict';
 
 systemDictionary = {
-    "Attention":
-    {
-        "en": "adapter, sbfspot and database should run on the same system",
-        "de": "adapter, sbfspot und database sollten auf demselben system laufen",
-        "ru": "адаптер, sbfspot и база данных должны работать в одной системе",
-        "pt": "adaptador, sbfspot e banco de dados devem ser executados no mesmo sistema",
-        "nl": "adapter, sbfspot en database moeten op hetzelfde systeem worden uitgevoerd",
-        "fr": "L'adaptateur, sbfspot et la base de données doivent fonctionner sur le même système",
-        "it": "adattatore, sbfspot e database dovrebbero essere eseguiti sullo stesso sistema",
-        "es": "adaptador, sbfspot y base de datos deben ejecutarse en el mismo sistema",
-        "pl": "adapter, sbfspot i baza danych powinny działać w tym samym systemie"
-    },
-    "sbfspot_DB_IP_descr":
-    {
-        "en": "you can use 'localhost' as IP",
-        "de": "Sie können \"localhost\" als IP verwenden",
-        "ru": "вы можете использовать «localhost» в качестве IP-адреса",
-        "pt": "você pode usar 'localhost' como IP",
-        "nl": "u kunt 'localhost' als IP gebruiken",
-        "fr": "vous pouvez utiliser 'localhost' comme adresse IP",
-        "it": "puoi usare 'localhost' come IP",
-        "es": "puedes usar 'localhost' como IP",
-        "pl": "możesz użyć \"localhost\" jako IP"
-    },
-    "sbfspot_DB_port_descr":
-    {
-        "en": "default 3306",
-        "de": "Standard 3306",
-        "ru": "по умолчанию 3306",
-        "pt": "padrão 3306",
-        "nl": "standaard 3306",
-        "fr": "par défaut 3306",
-        "it": "predefinito 3306",
-        "es": "predeterminado 3306",
-        "pl": "domyślnie 3306"
-    },
-    "path_to_DB_file_descr":
-    {
-        "en": "enter the complete path incl. database file name e.g. /home/pi/sbfspot/smadata/SBFspot.db",
-        "de": "geben Sie den kompletten Pfad inkl. Datenbankdateiname z.B. /home/pi/sbfspot/smadata/SBFspot.db",
-        "ru": "введите полный путь, включая. имя файла базы данных, например. /home/pi/sbfspot/smadata/SBFspot.db",
-        "pt": "insira o caminho completo incl. nome do arquivo do banco de dados, e. /home/pi/sbfspot/smadata/SBFspot.db",
-        "nl": "vul het complete pad in incl. databasebestandsnaam, b.v. /home/pi/sbfspot/smadata/SBFspot.db",
-        "fr": "entrez le chemin complet incl. nom de fichier de base de données, par ex. /home/pi/sbfspot/smadata/SBFspot.db",
-        "it": "inserisci il percorso completo incl. nome del file di database, ad es. /home/pi/sbfspot/smadata/SBFspot.db",
-        "es": "ingrese la ruta completa incl. nombre de archivo de base de datos, p. /home/pi/sbfspot/smadata/SBFspot.db",
-        "pl": "wprowadź pełną ścieżkę włącznie nazwa pliku bazy danych, np. /home/pi/sbfspot/smadata/SBFspot.db"
-    },
-    "install_date":
-    {
-        "en": "install date PV-System",
-        "de": "Installationsdatum PV-Anlage",
-        "ru": "дата установки PV-система",
-        "pt": "data de instalação PV-System",
-        "nl": "installeer datum PV-systeem",
-        "fr": "date d'installation PV-System",
-        "it": "installare data PV-System",
-        "es": "fecha de instalación PV-System",
-        "pl": "data instalacji PV-System"
-    },
-    "install_date_desc":
-    {
-        "en": "used for calculation of history in vis widget",
-        "de": "verwendet für die Berechnung der Geschichte in vis Widget",
-        "ru": "используется для расчета истории в виджетах",
-        "pt": "usado para calcular o histórico no vis widget",
-        "nl": "gebruikt voor de berekening van de geschiedenis in vis widget",
-        "fr": "utilisé pour le calcul de l'histoire dans vis widget",
-        "it": "utilizzato per il calcolo della cronologia nel widget vis",
-        "es": "utilizado para el cálculo de la historia en el widget vis",
-        "pl": "służy do obliczania historii w widgecie vis"
-    },
-
-    "path_to_DB_file":
-    {
-        "en": "path to sqlite database file",
-        "de": "Pfad zur SQLite-Datenbankdatei",
-        "ru": "путь к файлу базы данных sqlite",
-        "pt": "caminho para o arquivo de banco de dados sqlite",
-        "nl": "pad naar sqlite databasebestand",
-        "fr": "chemin vers le fichier de base de données sqlite",
-        "it": "percorso per il file di database sqlite",
-        "es": "ruta al archivo de base de datos sqlite",
-        "pl": "ścieżka do pliku bazy danych sqlite"
-    },
-    "sbfspot_DB_IP":
-    {
-        "en": "database IP",
-        "de": "Datenbank-IP",
-        "ru": "IP-адрес",
-        "pt": "banco de dadosL",
-        "nl": "database IP",
-        "fr": "base de données IP",
-        "it": "IP del databaseL",
-        "es": "base de datos IP",
-        "pl": "IP bazy danych"
-    },
-    "sbfspot_DB_port":
-    {
-        "en": "database port",
-        "de": "Datenbankport",
-        "ru": "Порт базы данных",
-        "pt": "Porta da base de dados",
-        "nl": "MariaDB databasepoort",
-        "fr": "Port de base de données",
-        "it": "Porta del database",
-        "es": "Puerto de base de datos",
-        "pl": "Port bazy danych"
-    },
-    "sbfspot_DB_databasename":
-    {
-        "en": "database name",
-        "de": "Datenbank-Name",
-        "ru": "Имя базы данных",
-        "pt": "Nome do banco de dados",
-        "nl": "databasenaam",
-        "fr": "Nom de la base de données",
-        "it": "nome del database",
-        "es": "nombre de la base de datos",
-        "pl": "nazwa bazy danych"
-    },
-    "sbfspot_DB_password":
-    {
-        "en": "database password",
-        "de": "Datenbankkennwort",
-        "ru": "пароль базы данных",
-        "pt": "senha do banco de dados",
-        "nl": "database wachtwoord",
-        "fr": "Mot de passe de base de données",
-        "it": "password del database",
-        "es": "contraseña de la base de datos",
-        "pl": "Hasło bazy danychL"
-    },
-    "sbfspot_DB_user":
-{
-        "en": "database user",
-        "de": "Benutzer der Datenbank",
-        "ru": "Пользователь базы данных ",
-        "pt": "usuário do banco de dados",
-        "nl": "mySQL-databasegebruiker",
-        "fr": "utilisateur de base de données",
-        "it": "utente del database",
-        "es": "usuario de la base de datos",
-        "pl": "użytkownik bazy danych"
-    },
-    "sbfspot_adapter_settings":
-{
-        "en": "sbfspot adapter settings",
-        "de": "sbfspot Adapter Einstellungen",
-        "ru": "Настройки адаптера sbfspot",
-        "pt": "Configurações do adaptador sbfspot",
-        "nl": "sbfspot-adapterinstellingen",
-        "fr": "Paramètres de l'adaptateur sbfspot",
-        "it": "impostazioni dell'adattatore sbfspot",
-        "es": "Configuración del adaptador sbfspot",
-        "pl": "Ustawienia adaptera sbfspot"
-    },
-    "type_of_DB": {
-        "en": "type of database",
-        "de": "Type der Datenbank",
-        "ru": "тип базы данных",
-        "pt": "tipo de banco de dados",
-        "nl": "type database",
-        "fr": "type de base de données",
-        "it": "tipo di database",
-        "es": "tipo de base de datos",
-        "pl": "typ bazy danych"
-    },
-    "GetDataOnlyWhenDaylight": {
-        "en": "get data only when daylight",
-        "de": "Daten nur bei Tageslicht abrufen",
-        "ru": "получать данные только при дневном свете",
-        "pt": "obtenha dados somente quando a luz do dia",
-        "nl": "krijg alleen gegevens bij daglicht",
-        "fr": "obtenir des données uniquement lorsque la lumière du jour",
-        "it": "ottenere i dati solo alla luce del giorno",
-        "es": "obtener datos solo cuando la luz del día",
-        "pl": "odbieraj dane tylko przy świetle dziennym",
-        "zh-cn": "仅在白天时获取数据"
-    }
+    "Attention": {                                   "en": "adapter, sbfspot and database should run on the same system", "de": "adapter, sbfspot und database sollten auf demselben system laufen", "ru": "адаптер, sbfspot и база данных должны работать в одной системе", "pt": "adaptador, sbfspot e banco de dados devem ser executados no mesmo sistema", "nl": "adapter, sbfspot en database moeten op hetzelfde systeem worden uitgevoerd", "fr": "L'adaptateur, sbfspot et la base de données doivent fonctionner sur le même système", "it": "adattatore, sbfspot e database dovrebbero essere eseguiti sullo stesso sistema", "es": "adaptador, sbfspot y base de datos deben ejecutarse en el mismo sistema", "pl": "adapter, sbfspot i baza danych powinny działać w tym samym systemie", "zh-cn": "适配器，sbfspot和数据库应在同一系统上运行"},
+    "GetDataOnlyWhenDaylight": {                     "en": "get data only when daylight",                     "de": "Daten nur bei Tageslicht abrufen",                "ru": "получать данные только при дневном свете",        "pt": "obtenha dados somente quando a luz do dia",       "nl": "krijg alleen gegevens bij daglicht",              "fr": "obtenir des données uniquement lorsque la lumière du jour", "it": "ottenere i dati solo alla luce del giorno",       "es": "obtener datos solo cuando la luz del día",        "pl": "odbieraj dane tylko przy świetle dziennym",       "zh-cn": "仅在白天时获取数据"},
+    "install_date": {                                "en": "install date PV-System",                          "de": "Installationsdatum PV-Anlage",                    "ru": "дата установки PV-система",                       "pt": "data de instalação PV-System",                    "nl": "installeer datum PV-systeem",                     "fr": "date d'installation PV-System",                   "it": "installare data PV-System",                       "es": "fecha de instalación PV-System",                  "pl": "data instalacji PV-System",                       "zh-cn": "安装日期PV-System"},
+    "install_date_desc": {                           "en": "used for calculation of history in vis widget",   "de": "verwendet für die Berechnung der Geschichte in vis Widget", "ru": "используется для расчета истории в виджетах",     "pt": "usado para calcular o histórico no vis widget",   "nl": "gebruikt voor de berekening van de geschiedenis in vis widget", "fr": "utilisé pour le calcul de l'histoire dans vis widget", "it": "utilizzato per il calcolo della cronologia nel widget vis", "es": "utilizado para el cálculo de la historia en el widget vis", "pl": "służy do obliczania historii w widgecie vis",     "zh-cn": "用于计算vis小部件中的历史记录"},
+    "path_to_DB_file": {                             "en": "path to sqlite database file",                    "de": "Pfad zur SQLite-Datenbankdatei",                  "ru": "путь к файлу базы данных sqlite",                 "pt": "caminho para o arquivo de banco de dados sqlite", "nl": "pad naar sqlite databasebestand",                 "fr": "chemin vers le fichier de base de données sqlite", "it": "percorso per il file di database sqlite",         "es": "ruta al archivo de base de datos sqlite",         "pl": "ścieżka do pliku bazy danych sqlite",             "zh-cn": "sqlite数据库文件的路径"},
+    "path_to_DB_file_descr": {                       "en": "enter the complete path incl. database file name e.g. /home/pi/sbfspot/smadata/SBFspot.db", "de": "geben Sie den kompletten Pfad inkl. Datenbankdateiname z.B. /home/pi/sbfspot/smadata/SBFspot.db", "ru": "введите полный путь, включая. имя файла базы данных, например. /home/pi/sbfspot/smadata/SBFspot.db", "pt": "insira o caminho completo incl. nome do arquivo do banco de dados, e. /home/pi/sbfspot/smadata/SBFspot.db", "nl": "vul het complete pad in incl. databasebestandsnaam, b.v. /home/pi/sbfspot/smadata/SBFspot.db", "fr": "entrez le chemin complet incl. nom de fichier de base de données, par ex. /home/pi/sbfspot/smadata/SBFspot.db", "it": "inserisci il percorso completo incl. nome del file di database, ad es. /home/pi/sbfspot/smadata/SBFspot.db", "es": "ingrese la ruta completa incl. nombre de archivo de base de datos, p. /home/pi/sbfspot/smadata/SBFspot.db", "pl": "wprowadź pełną ścieżkę włącznie nazwa pliku bazy danych, np. /home/pi/sbfspot/smadata/SBFspot.db", "zh-cn": "输入完整的路径，包括"},
+    "sbfspot_DB_IP": {                               "en": "database IP",                                     "de": "Datenbank-IP",                                    "ru": "IP-адрес",                                        "pt": "banco de dadosL",                                 "nl": "database IP",                                     "fr": "base de données IP",                              "it": "IP del databaseL",                                "es": "base de datos IP",                                "pl": "IP bazy danych",                                  "zh-cn": "数据库IP"},
+    "sbfspot_DB_IP_descr": {                         "en": "you can use 'localhost' as IP",                   "de": "Sie können \"localhost\" als IP verwenden",       "ru": "вы можете использовать «localhost» в качестве IP-адреса", "pt": "você pode usar 'localhost' como IP",              "nl": "u kunt 'localhost' als IP gebruiken",             "fr": "vous pouvez utiliser 'localhost' comme adresse IP", "it": "puoi usare 'localhost' come IP",                  "es": "puedes usar 'localhost' como IP",                 "pl": "możesz użyć \"localhost\" jako IP",               "zh-cn": "您可以使用“本地主机”作为IP"},
+    "sbfspot_DB_databasename": {                     "en": "database name",                                   "de": "Datenbank-Name",                                  "ru": "Имя базы данных",                                 "pt": "Nome do banco de dados",                          "nl": "databasenaam",                                    "fr": "Nom de la base de données",                       "it": "nome del database",                               "es": "nombre de la base de datos",                      "pl": "nazwa bazy danych",                               "zh-cn": "数据库名称"},
+    "sbfspot_DB_password": {                         "en": "database password",                               "de": "Datenbankkennwort",                               "ru": "пароль базы данных",                              "pt": "senha do banco de dados",                         "nl": "database wachtwoord",                             "fr": "Mot de passe de base de données",                 "it": "password del database",                           "es": "contraseña de la base de datos",                  "pl": "Hasło bazy danychL",                              "zh-cn": "数据库密码"},
+    "sbfspot_DB_port": {                             "en": "database port",                                   "de": "Datenbankport",                                   "ru": "Порт базы данных",                                "pt": "Porta da base de dados",                          "nl": "MariaDB databasepoort",                           "fr": "Port de base de données",                         "it": "Porta del database",                              "es": "Puerto de base de datos",                         "pl": "Port bazy danych",                                "zh-cn": "数据库端口"},
+    "sbfspot_DB_port_descr": {                       "en": "default 3306",                                    "de": "Standard 3306",                                   "ru": "по умолчанию 3306",                               "pt": "padrão 3306",                                     "nl": "standaard 3306",                                  "fr": "par défaut 3306",                                 "it": "predefinito 3306",                                "es": "predeterminado 3306",                             "pl": "domyślnie 3306",                                  "zh-cn": "默认值3306"},
+    "sbfspot_DB_user": {                             "en": "database user",                                   "de": "Benutzer der Datenbank",                          "ru": "Пользователь базы данных ",                       "pt": "usuário do banco de dados",                       "nl": "mySQL-databasegebruiker",                         "fr": "utilisateur de base de données",                  "it": "utente del database",                             "es": "usuario de la base de datos",                     "pl": "użytkownik bazy danych",                          "zh-cn": "数据库用户"},
+    "sbfspot_adapter_settings": {                    "en": "sbfspot adapter settings",                        "de": "sbfspot Adapter Einstellungen",                   "ru": "Настройки адаптера sbfspot",                      "pt": "Configurações do adaptador sbfspot",              "nl": "sbfspot-adapterinstellingen",                     "fr": "Paramètres de l'adaptateur sbfspot",              "it": "impostazioni dell'adattatore sbfspot",            "es": "Configuración del adaptador sbfspot",             "pl": "Ustawienia adaptera sbfspot",                     "zh-cn": "sbfspot适配器设置"},
+    "type_of_DB": {                                  "en": "type of database",                                "de": "Type der Datenbank",                              "ru": "тип базы данных",                                 "pt": "tipo de banco de dados",                          "nl": "type database",                                   "fr": "type de base de données",                         "it": "tipo di database",                                "es": "tipo de base de datos",                           "pl": "typ bazy danych",                                 "zh-cn": "数据库类型"},
 };
